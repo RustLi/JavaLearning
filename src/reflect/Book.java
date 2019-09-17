f9defbdebc6b0e0d5a1153956bc18a2ab678ae8b
RustLi <332014208@qq.com>
2019-09-17 14:07:24 +0800

    add:反射示例

---
+++ src/reflect/Book.java
@@ -0,0 +1,59 @@
+package reflect;
+
+public class Book {
+
+    static final String TAG = "book";
+
+    private String name;
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    private String declaredMethod(int index) {
+        String string = null;
+        switch (index) {
+            case 0:
+                string = "I am declaredMethod 1 !";
+                break;
+            case 1:
+                string = "I am declaredMethod 2 !";
+                break;
+            default:
+                string = "I am declaredMethod 1 !";
+        }
+
+        return string;
+    }
+
+
+}
---
+++ src/reflect/ReflectObject.java
@@ -0,0 +1,83 @@
+package reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ReflectObject {
+
+    public static void main(String[] args) {
+        try {
+            // 创建对象
+            ReflectObject.reflectNewInstance();
+
+            // 反射私有的构造方法
+            ReflectObject.reflectPrivateConstructor();
+
+            // 反射私有属性
+            ReflectObject.reflectPrivateField();
+
+            // 反射私有方法
+            ReflectObject.reflectPrivateMethod();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    // 创建对象
+    private static void reflectNewInstance() {
+        try {
+            Class<?> classBook = Class.forName("reflect.Book");
+            Object objectBook = classBook.newInstance();
+            Book book = (Book) objectBook;
+            book.setName("Android进阶之光");
+            book.setAuthor("刘望舒");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // 反射私有的构造方法
+    private static void reflectPrivateConstructor() {
+        try {
+            Class<?> classBook = Class.forName("reflect.Book");
+            Constructor<?> declaredConstructorBook = classBook.getDeclaredConstructor(String.class,String.class);
+            declaredConstructorBook.setAccessible(true);
+            Object objectBook = declaredConstructorBook.newInstance("Android开发艺术探索","任玉刚");
+            Book book = (Book) objectBook;
+            System.out.println("book name is " + book.getName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // 反射私有属性
+    private static void reflectPrivateField() {
+        try {
+            Class<?> classBook = Class.forName("reflect.Book");
+            Object objectBook = classBook.newInstance();
+            Field fieldTag = classBook.getDeclaredField("TAG");
+            fieldTag.setAccessible(true);
+            String tag = (String) fieldTag.get(objectBook);
+            System.out.println("tag is " + tag);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // 反射私有方法
+    private static void reflectPrivateMethod() {
+        try {
+            Class<?> classBook = Class.forName("reflect.Book");
+            Method methodBook = classBook.getDeclaredMethod("declaredMethod",int.class);
+            methodBook.setAccessible(true);
+            Object objectBook = classBook.newInstance();
+            String str = (String) methodBook.invoke(objectBook,0);
+            System.out.println("str is " + str);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+}
